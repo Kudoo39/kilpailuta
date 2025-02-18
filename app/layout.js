43914a630842a0204f214bba8c25c7af80d17625
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2025-02-18 08:55:31 +0200

    Add pages

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,15 @@
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin']
-});
+})
 
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin']
-});
+})
 
 export const metadata = {
   title: 'Kilpailuta',
@@ -18,12 +18,12 @@ export const metadata = {
     icon: '/jobfinder.ico',
     shortcut: '/jobfinder.ico'
   }
-};
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body>{children}</body>
     </html>
-  );
+  )
 }
